05217b3fff4aa1cdc1f0d9b00da05a311be72c13
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-04-06 08:01:10 +0100

    Initial commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+<title>About the Sveletkit pokedex</title>
+</svelte:head>
+
+<h1>About</h1>
+<p>This is good using pokemon api</p>
---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script>
+import Nav from "../components/nav.svelte"
+console.log("hello world")</script>
+<svelte:head>
+<title>Sveletkit pokedex</title>
+</svelte:head>
+<h1>SveleteKit Pokedex</h1>
+
+
